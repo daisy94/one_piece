9b734164464078c6d141c9308a6feec74d9c7c89
daisy94 <1015990474@qq.com>
2020-08-25 09:47:06 +0800

    删除了不要的文件

---
+++ hushang_ayi/src/main/webapp/resources/utils/utils.js
@@ -0,0 +1,30 @@
+function formatDate(date){
+
+    let str = String();
+    let arr = date.split('-');
+    for(let i=0;i<arr.length;i++){
+        str += arr[i];
+    }
+    return str
+}
+
+function formatDateMonth(date){
+
+    let month = String();
+    if(date < 10){
+
+        month = "0" + date;
+        return month;
+    }else{
+
+        month = date;
+        return month;
+    }
+}
+
+function isNumber(val){
+
+    let regPos = /^\d+(\.\d+)?$/; //非负浮点数
+    let regNeg = /^(-(([0-9]+\.[0-9]*[1-9][0-9]*)|([0-9]*[1-9][0-9]*\.[0-9]+)|([0-9]*[1-9][0-9]*)))$/; //负浮点数
+    return regPos.test(val) || regNeg.test(val);
+}
